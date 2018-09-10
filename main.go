820c51532a531bb12027b2dfac6eb22f73d8f31f
0GGmr0 <18817619557@163.com>
2018-09-10 20:48:17 +0800

    并发读入版本，测试速度和并发速度就差8秒 妈耶

--- main.go
+++ main.go
@@ -1,32 +1,63 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"time"
+)
 
-const NUMBER_NUM = 1000000
-
+const GenRoutineNum = 1000
+const MaxNumber = 100000
 func main() {
-	
+	t := time.Now()
+	var channels [GenRoutineNum]<-chan int
+	for i := 0; i < GenRoutineNum; i++ {
+		channels[i] = createWorker(i)
+	}
+	count := 0
+	for i := 0; i < GenRoutineNum; i++ {
+		for v := range channels[i] {
+			judgePrimeNumber(v , &count)
+		}
+
+	}
+	fmt.Println("*******")
+	fmt.Println(count)
+	elapsed := time.Since(t)
+	fmt.Println("all time: ", elapsed)
 }
 
-func generate
 
-
-func judgePrimeNumber(in <-chan int) <-chan int {
-	out := make(chan int)
-	number := <- in
+//创建channel
+func createWorker(i int) <-chan int {
+	c := make(chan int, MaxNumber)
 	go func() {
-		if number == 2 || number == 3 {
-			out <- number
+		for j := i * MaxNumber; j < (i+1)*MaxNumber; j++ {
+			c <- j
 		}
-		if number % 6 == 1 || number % 6 == 5 {
-			tmp := int(math.Sqrt(float64(number)))
-			for i := 5; i <= tmp; i++ {
-				if number % i == 0 || number % (i + 2) == 0 {
-					break
-				}
-			}
-			out <- number
-		}
+		close(c)
 	}()
-	return out
+	return c
+}
+
+
+//判断素数
+func judgePrimeNumber(number int, count *int) {
+	if number == 1 ||
+		number%6 != 1 && number%6 != 5 {
+		return
+	}
+	if number == 2 || number == 3 {
+		*count++
+		fmt.Println(number)
+		return
+	}
+	tmp := int(math.Sqrt(float64(number)))
+	for i := 5; i <= tmp; i += 6 {
+		if number%i == 0 || number%(i+2) == 0 {
+			return
+		}
+	}
+	fmt.Println(number)
+	*count++
 }
---
+++ test/test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	count := 0
+	for i := 0; i < 100000000; i++ {
+		judgePrimeNumber(i, &count)
+	}
+	fmt.Println("*****************")
+	fmt.Println(count)
+	elapsed := time.Since(t)
+	fmt.Println("all time: ", elapsed)
+}
+
+func judgePrimeNumber(number int, count *int) {
+	if number == 1 ||
+		number%6 != 1 && number%6 != 5 {
+		return
+	}
+	if number == 2 || number == 3 {
+		*count++
+		fmt.Println(number)
+		return
+	}
+	tmp := int(math.Sqrt(float64(number)))
+	for i := 5; i <= tmp; i += 6 {
+		if number%i == 0 || number%(i+2) == 0 {
+			return
+		}
+		//排除所有，剩余的是质数
+	}
+	*count++
+	fmt.Println(number)
+}
